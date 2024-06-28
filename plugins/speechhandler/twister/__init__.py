ed6b819f2d7f21a721f0c6d2c9d2c98c79751346
Michelle Timmons <michelle.timmons@maystanley.co.uk>
2024-06-28 22:00:29 +0100

    Twister plugin

---
+++ plugins/speechhandler/twister/__init__.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+from naomi import plugin
+
+
+# The speechhandler plugin represents something that Naomi does
+# in response to a request from the user. This is often a spoken
+# response, but can also be an action like turning on a light or
+# sending an email. It is the functional equivalent of a skill on
+# most other assistant platforms.
+# For details about writing a speech handler, see:
+# https://projectnaomi.com/dev/docs/developer/plugins/speechhandler_plugin.html
+class MySpeechHandler(plugin.SpeechHandlerPlugin):
+    # Intents describe how your plugin may be activated.
+    # At the simplest level, just write all the things you think
+    # someone might say if they wanted to activate your
+    # plugin. Finally, supply a link to the handle method,
+    # which Naomi will use when your intent is selected.
+    def intents(self):
+        return {
+            'TwisterIntent': {
+                'locale': {
+                    'en-US': {
+                        'templates': [
+                           "Twister"
+                        ]
+                    }
+                },
+                'action': self.handle
+            }
+        }
+
+    # The handle method is where you pick up after Naomi has
+    # identified your intent as the one the user was attempting
+    # to activate.
+    def handle(self, intent, mic):
+        # The intent parameter is a structure with information about
+        # the user request. intent['input'] will hold the transcription
+        # of the user's request.
+        # text = intent['input']
+
+        # The mic parameter is a microphone object that you can
+        # use to respond to the user.
+        # mic.say("You said, {}".format(text))
+
+        mic.say("OK, let's play Twister")
+
+        # TODO: disable the ambient plugin...? or should the ambient plugin disable itself when other plugins are active? 
+        # TODO: start a loop
+        # TODO: randomly select a colour and a hand/foot
+        # TODO: say the instruction
+        # TODO: respond to "game over" to quit the loop
---
+++ plugins/speechhandler/twister/readme.md
@@ -0,0 +1,22 @@
+---
+id: twister
+label: Twister
+title: Twister - Speechhandler
+type: speechhandlers
+description: "Play Twister"
+logo: images/plugins/
+source: https://github.com/michelletimmons/NaomiFurbyblob/master/readme.md
+meta:
+  - property: og:title
+    content: "Twister - Speechhandler"
+  - property: og:description
+    content: "Play Twister"
+---
+
+# Twister - Speechhandler
+
+<PluginLogo/>
+
+Play Twister
+
+<EditPageLink/>
